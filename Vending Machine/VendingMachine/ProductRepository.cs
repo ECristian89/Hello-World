838c618e7af8ef68b88a3b5b920e311580421c93
Cristian <caene89@gmail.com>
2022-12-06 21:56:30 +0200

    -added  Look and Block Diagrams -added Product class.cs -added ProductRepository.cs -added LookUseCase.cs

---
+++ Vending Machine/VendingMachine/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iQuest.VendingMachine
+{
+    internal class ProductRepository
+    {
+        private List<Product> Products;
+        public IEnumerable<Product> GetAll()
+        {
+            return Products;
+        }
+    }
+}
---
+++ Vending Machine/VendingMachine/UseCases/LookUseCase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace iQuest.VendingMachine.UseCases
+{
+    internal class LookUseCase : IUseCase
+    {
+        public string Name => "look";
+
+        public string Description => "See the products list";
+
+        public bool CanExecute => throw new NotImplementedException();
+
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
